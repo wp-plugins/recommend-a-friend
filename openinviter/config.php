b5fc9ee934c5c69a2a2c3e35cc4a2aad9800d158
benjaminniess <benjaminniess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-04 14:32:32 +0000

    Release 1.0.1 version
    git-svn-id: https://plugins.svn.wordpress.org/recommend-a-friend/tags/1.0.1@380767 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ openinviter/config.php
@@ -0,0 +1,5 @@
+<?php
+$openinviter_settings=array(
+'username'=>"benjaminniess", 'private_key'=>"b6c3d5254f98781e5c4e1473e6931566", 'cookie_path'=>"/tmp", 'message_body'=>"Vous êtes invité sur www.beapi.fr", 'message_subject'=>" vous invite sur www.beapi.fr", 'transport'=>"curl", 'local_debug'=>"on_error", 'remote_debug'=>"", 'hosted'=>"", 'proxies'=>array(),
+'stats'=>"", 'plugins_cache_time'=>"1800", 'plugins_cache_file'=>"oi_plugins.php", 'update_files'=>"1", 'stats_user'=>"", 'stats_password'=>"");
+?>
